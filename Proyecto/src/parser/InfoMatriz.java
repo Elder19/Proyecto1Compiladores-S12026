fb4cd4b471800d36c9e0ab26c3671085e1f7bc5e
ELDERPEREZ <125334333+Elder19@users.noreply.github.com>
2026-06-15 18:22:40 -0600

    errores

--- Proyecto/src/parser/InfoMatriz.java
+++ Proyecto/src/parser/InfoMatriz.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class InfoMatriz {
     public int filas;
     public int columnas;
     public boolean error;
+    public List valores;
 
     public InfoMatriz(int filas, int columnas) {
         this.filas = filas;
         this.columnas = columnas;
         this.error = false;
+        this.valores = new ArrayList();
     }
 
     public InfoMatriz(int filas, int columnas, boolean error) {
         this.filas = filas;
         this.columnas = columnas;
         this.error = error;
+        this.valores = new ArrayList();
+    }
+
+    public InfoMatriz(int filas, int columnas, boolean error, List valores) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.error = error;
+        this.valores = valores;
     }
 }
